76892d6e897e8346b5afb5c100177f9159c8e15f
sarayupyla <sarayupyla@gmail.com>
2026-05-08 20:15:38 +0530

    solved

--- SLIDING WINDOW/1423MaximumPointsYouCanObtainfromCards.py
+++ SLIDING WINDOW/1423MaximumPointsYouCanObtainfromCards.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         n=len(cardPoints)
-        if k==n:
+        if k==n:  #k value is same as len of cardpoints then we can do sum of all cards and return
             return sum(cardPoints)
-        total_sum=sum(cardPoints)
-        win_size=n-k
-        win_sum=sum(cardPoints[:win_size])
-        min_sum=win_sum
+        total_sum=sum(cardPoints) #used later for subtracting
+        win_size=n-k  
+        win_sum=sum(cardPoints[:win_size]) #sum of first winsize elements
+        min_sum=win_sum #to know min value to subtract total-min to get required result
         left=0
         for right in range(win_size,n):
-            win_sum+=cardPoints[right]
-            win_sum-=cardPoints[left]
-            left+=1
-            min_sum=min(min_sum,win_sum)
+            win_sum+=cardPoints[right]  # moving the window forward by adding the right element
+            win_sum-=cardPoints[left] #while window is moving forward the left pointer is shrinked
+            left+=1  #after shrinking move left pointer forward
+            min_sum=min(min_sum,win_sum)  #update min value
         return total_sum-min_sum
       
