6a2f447c302e802de77b080a8d547d1ca46aa2d0
sarayupyla <sarayupyla@gmail.com>
2026-08-20 13:02:07 +0530

    solved using left join

---
+++ SQL 50/1075ProjectEmployees_I.sql
@@ -0,0 +1,6 @@
+select Project.project_id,
+round(avg(Employee.experience_years),2) as average_years
+from project
+left join Employee
+on Project.employee_id=Employee.employee_id
+group by Project.project_id;
